154a3b01a31012534b082ab2051810d14075c054
Janvi Umendra <74888669+uuuuju@users.noreply.github.com>
2022-05-29 10:34:24 +0530

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,39 @@
+/* pages/_app.js */
 import '../styles/globals.css'
+import Link from 'next/link'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <nav className="border-b p-6">
+        <p className="text-6xl text-orange-900 text-center font-bold">WoodeNFT</p>
+        <div className="flex mt-4">
+          <Link href="/">
+            <a className="mr-4 text-sky-500">
+              Home
+            </a>
+          </Link>
+          <Link href="/create-nft">
+            <a className="mr-4 text-sky-500">
+              Sell NFT
+            </a>
+          </Link>
+          <Link href="/my-nfts">
+            <a className="mr-4 text-sky-500">
+              My NFTs
+            </a>
+          </Link>
+          <Link href="/dashboard">
+            <a className="mr-4 text-sky-500">
+              Dashboard
+            </a>
+          </Link>
+        </div>
+      </nav>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
+
